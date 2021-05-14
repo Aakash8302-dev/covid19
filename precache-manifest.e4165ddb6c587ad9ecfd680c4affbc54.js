2b685d2e8f6987e0a4a91333d842f06330f477e9
Aakash Arumugam <aakasharumugam18@gmail.com>
2021-05-14 15:41:26 +0530

    Updates

---
+++ precache-manifest.e4165ddb6c587ad9ecfd680c4affbc54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "85a3b98b5e22f2300f93",
+    "url": "/covid19/static/css/main.05f914b9.chunk.css"
+  },
+  {
+    "revision": "85a3b98b5e22f2300f93",
+    "url": "/covid19/static/js/main.844732cb.chunk.js"
+  },
+  {
+    "revision": "9edb58dfee5d88d19d35",
+    "url": "/covid19/static/js/runtime~main.60db82c8.js"
+  },
+  {
+    "revision": "9b88ae839a49781fdafb",
+    "url": "/covid19/static/js/2.403c09a6.chunk.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19/static/media/image.d7265326.png"
+  },
+  {
+    "revision": "259a11f436aaa6cc3c4203fa45cde523",
+    "url": "/covid19/index.html"
+  }
+];
